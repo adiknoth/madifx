ab30fa95cef1a4727f8cc8440523aa2bcc010576
Adrian Knoth <adi@drcomp.erfurt.thur.de>
2015-02-16 11:20:57 +0100

    ALSA: Use setup_timer() and mod_timer()
    Backported from mainline kernel 04018e137c298f199fbf0f0c431d975f584f4702

--- madifx.c
+++ madifx.c
@@ -1037,10 +1037,8 @@ static void snd_madifx_midi_output_timer(unsigned long data)
 	   leaving istimer wherever it was set before.
 	*/
 
-	if (hmidi->istimer) {
-		hmidi->timer.expires = 1 + jiffies;
-		add_timer(&hmidi->timer);
-	}
+	if (hmidi->istimer)
+		mod_timer(&hmidi->timer, 1 + jiffies);
 
 	spin_unlock_irqrestore(&hmidi->lock, flags);
 }
@@ -1055,11 +1053,9 @@ snd_madifx_midi_output_trigger(struct snd_rawmidi_substream *substream, int up)
 	spin_lock_irqsave(&hmidi->lock, flags);
 	if (up) {
 		if (!hmidi->istimer) {
-			init_timer(&hmidi->timer);
-			hmidi->timer.function = snd_madifx_midi_output_timer;
-			hmidi->timer.data = (unsigned long) hmidi;
-			hmidi->timer.expires = 1 + jiffies;
-			add_timer(&hmidi->timer);
+			setup_timer(&hmidi->timer, snd_madifx_midi_output_timer,
+					(unsigned long) hmidi);
+			mod_timer(&hmidi->timer, 1 + jiffies);
 			hmidi->istimer++;
 		}
 	} else {
